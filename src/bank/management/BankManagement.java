08d3fb1b4e40ab2c735a4c495fd85fe02c4aba1a
Rejaul Karim Tirtho <ektirtho@gmail.com>
2018-11-26 15:09:33 +0600

    database creating

--- src/bank/management/BankManagement.java
+++ src/bank/management/BankManagement.java
@@ -5,6 +5,9 @@
  */
 package bank.management;
 
+import com.account.BankAccount;
+import com.account.CheckingAccount;
+import com.account.SavingsAccount;
 import com.client.Client;
 import com.util.Rom;
 import java.util.ArrayList;
@@ -64,6 +67,52 @@ public class BankManagement {
             
             
         }
+    }
+    
+    
+    
+    
+    
+    private static void initialDatabase(){
+    BankAccount svAccount1 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount2 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount3 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount4 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount5 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount6 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount7 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount8 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount9 = new SavingsAccount("SVS-220 424 12353", 30000);
+    BankAccount svAccount0 = new SavingsAccount("SVS-220 424 12353", 30000);
+   
+    BankAccount chAccount0 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount1 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount2 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount3 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount4 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount5 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount6 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount7 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount8 = new CheckingAccount("CHK-", 0);
+    BankAccount chAccount9 = new CheckingAccount("CHK-", 0);
+   
+    
+    BankAccount crAccount0=new CheckingAccount("CR", 0);
+    BankAccount crAccount1=new CheckingAccount("CR", 0);
+    BankAccount crAccount2=new CheckingAccount("CR", 0);
+    BankAccount crAccount3=new CheckingAccount("CR", 0);
+    BankAccount crAccount4=new CheckingAccount("CR", 0);
+    BankAccount crAccount5=new CheckingAccount("CR", 0);
+    BankAccount crAccount6=new CheckingAccount("CR", 0);
+    BankAccount crAccount7=new CheckingAccount("CR", 0);
+    BankAccount crAccount8=new CheckingAccount("CR", 0);
+    BankAccount crAccount9=new CheckingAccount("CR", 0);
+    
+    clients =new ArrayList<>();
+   
+//    Client c = new Client(userName, password, crAccount9, userName, userName, userName, userName, address);
+    
+    
     }
     
 }
